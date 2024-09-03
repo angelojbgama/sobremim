72eb2ae54f5475adc4bdeeddd21c63b5050a29b9
angelojbgama <angelojbgama@gmail.com>
2024-09-02 22:31:17 -0300

    mudança de profile photo

--- assets/js/acordeon.js
+++ assets/js/acordeon.js
@@ -1,13 +1,21 @@
-const acordeonTrigger = document.querySelectorAll('.acordeon .trigger')
+const acordeonTrigger = document.querySelectorAll('.acordeon .trigger');
 
-acordeonTrigger.forEach((trigger)=>{
-    trigger.addEventListener('click', (e)=>{
-        const acordeon = trigger.parentElement
-        const isOpen = acordeon.classList.contains('open')
-        if (isOpen) {
-            acordeon.classList.remove('open')     
-        }else {
-            acordeon.classList.add('open')
-        }
-    })
-})
+acordeonTrigger.forEach((trigger) => {
+  trigger.addEventListener('click', (e) => {
+    const acordeon = trigger.parentElement;
+    const content = acordeon.querySelector('.content');
+    const isOpen = acordeon.classList.contains('open');
+
+    if (isOpen) {
+      // Fechando o acordeão
+      content.style.maxHeight = 0;
+      content.style.opacity = 0;
+      acordeon.classList.remove('open');
+    } else {
+      // Abrindo o acordeão
+      content.style.maxHeight = content.scrollHeight + 'px';
+      content.style.opacity = 1;
+      acordeon.classList.add('open');
+    }
+  });
+});
